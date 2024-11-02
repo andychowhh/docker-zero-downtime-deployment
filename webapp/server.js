b3d6aefbff55fc3a4d850db18eae15f746227a93
Andy Chow <hohin523work@gmail.com>
2024-11-02 14:34:25 -0700

    remove unused code

--- webapp/server.js
+++ webapp/server.js
@@ -12,10 +12,6 @@ app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, 'index.html'));
 });
 
-app.get('/health', (req, res) => {
-  res.sendStatus(200);
-});
-
 app.listen(port, '0.0.0.0', () => {
   console.log(`Server is running on http://localhost:${port}`);
 });
